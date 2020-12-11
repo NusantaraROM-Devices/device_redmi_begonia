74d55fe0100f8fcf952aab31d97b840bcae1a5e0
xyzuan <i.xyzuan@gmail.com>
2020-12-11 11:08:08 +0000

    begonia: overlay-ancient: define begonia specs.

--- device.mk
+++ device.mk
@@ -140,7 +140,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-ancient
 
 # Permissions
 PRODUCT_COPY_FILES += \
